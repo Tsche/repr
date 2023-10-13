fcc6e1b8453d7948848e64ed20ca48e3be68e299
Tsche <Tsche@users.noreply.github.com>
2023-10-13 22:51:37 +0200

    fix rounding issue in test

--- test/repr/variant.cpp
+++ test/repr/variant.cpp
@@ -5,6 +5,6 @@
 TEST(repr, variant) {
   auto fundamental = std::variant<unsigned int, float>{42U};
   EXPECT_EQ(repr(fundamental), "std::variant<unsigned int, float>{42U}");
-  fundamental = 4.20F;
-  EXPECT_EQ(repr(fundamental), "std::variant<unsigned int, float>{4.20F}");
+  fundamental = 4.21F;
+  EXPECT_EQ(repr(fundamental), "std::variant<unsigned int, float>{4.21F}");
 }
