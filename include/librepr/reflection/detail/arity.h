cae517080afb1f40553bfe3e1173d9e181f6681d
André Polomat <monstrefou@gmail.com>
2023-10-23 13:43:15 -0400

    Added support for arrity of struct with C-array members

--- include/librepr/reflection/detail/arity.h
+++ include/librepr/reflection/detail/arity.h
@@ -2,19 +2,67 @@
 #include <type_traits>
 
 namespace librepr::detail {
+#include <type_traits>
+
 struct Universal {
-  template <typename T>
-  [[implicit]] operator T() {}
+    template <typename T>
+    constexpr operator T();
 };
 
 template <typename T>
-requires (std::is_aggregate_v<T> && !std::is_array_v<T>)
-consteval auto arity(auto... parameters) {
-  if constexpr (requires { T{parameters..., Universal{}}; }) {
-    return arity<T>(parameters..., Universal{});
-  } else {
-    return sizeof...(parameters);
-  }
-}
+    requires std::is_aggregate_v<T>
+struct ArityImpl {
+    template <typename... Fillers>
+    static consteval auto array_length(auto... parameters) {
+        if constexpr (requires { T{parameters..., {Fillers{}..., Universal{}}}; }) {
+            return array_length<Fillers..., Universal>(parameters...);
+        } else {
+            return sizeof...(Fillers);
+        }
+    }
+
+    static consteval auto arity_simple(auto... parameters) {
+        if constexpr (requires { T{parameters..., Universal{}}; }) {
+            return arity_simple(parameters..., Universal{});
+        } else {
+            return sizeof...(parameters);
+        }
+    }
+
+    template <typename... Trails>
+    static consteval auto arity_simple_ag(auto... parameters) {
+        if constexpr (
+            requires {
+                T{parameters..., {Universal{}, Universal{}}, Trails{}..., Universal{}};
+            }) {
+            return arity_simple_ag<Trails..., Universal>(parameters...);
+        } else {
+            return sizeof...(parameters) + sizeof...(Trails) + 1;
+        }
+    }
+
+    static consteval auto arity(int minus = 0, auto... parameters) {
+        if constexpr (
+            requires {
+                T{parameters..., {Universal{}, Universal{}}};
+            }) {
+            constexpr bool isArray =
+                arity_simple_ag(parameters...) != arity_simple(parameters...);
+            if constexpr (isArray) {
+                minus += array_length(parameters...) - 1;
+            }
+
+            return arity(minus, parameters..., Universal{});
+        }else if constexpr (requires { T{parameters..., Universal{}}; }) {
+            return arity(minus, parameters..., Universal{});
+        } else {
+            return sizeof...(parameters) - minus;
+        }
+    }
+};
+
+template <typename T>
+    requires std::is_aggregate_v<T>
+inline constexpr std::size_t arity = ArityImpl<T>::arity();
 
 }  // namespace librepr
