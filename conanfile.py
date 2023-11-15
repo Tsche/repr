f624e6db51306301f5c3c4856035483687607187
Tsche <Tsche@users.noreply.github.com>
2023-11-15 01:49:12 +0100

    adjust sonar config for header-only

--- conanfile.py
+++ conanfile.py
@@ -39,6 +39,7 @@ class ReprRecipe(ConanFile):
             cmake.build()
 
     def package(self):
+        # TODO copy <repr>
         copy(self, "*.h", self.source_folder, self.package_folder)
 
     def package_info(self):
